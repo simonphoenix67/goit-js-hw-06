7efe5852b08497d436cb90e2cbb259e94c848242
simonphoenix67 <simonphoenix67@gmail.com>
2023-01-24 20:23:18 +0200

    task02

--- js/task-02.js
+++ js/task-02.js
@@ -8,16 +8,37 @@ const ingredients = [
 ];
 
 
-function addin(ingredients) {
-    for (let i of ingredients) {
-      const addList = document.createElement('li');
-      const unList = document.querySelector('#ingredients');
-      addList.className = 'list';
-      addList.textContent = i;
-      unList.append(addList);
-    }
+
+
+const list = document.querySelector("#ingredients");
+
+const addin = [];
+
+ingredients.map(ingredient => {
+
+  const addList = document.createElement("li");
+
+  addList.className = 'item'; 
+
+  addList.textContent = ingredient;
+
+  addin.push(addList);
+})
+
+list.append(...addin);
+
+
+
+// function addin(ingredients) {
+//     for (let i of ingredients) {
+//       const addList = document.createElement('li');
+//       const unList = document.querySelector('#ingredients');
+//       addList.className = 'list';
+//       addList.textContent = i;
+//       unList.append(addList);
+//     }
   
-}
+// }
 
 
-addin(ingredients);
+// addin(ingredients);
